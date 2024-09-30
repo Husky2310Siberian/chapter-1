2945fef1ff57ca5447991c5adb314f912f85d903
Siberian <vasileioskritikos@aueb.gr>
2024-09-30 20:17:53 +0300

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ import java.net.SocketOption;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!");
         }
     }
