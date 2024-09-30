4a8fc560ba211d1ba27caf75013d46d12927d65f
haskeyvas <vasileios.kritikos@aueb.gr>
2024-09-30 19:51:42 +0300

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch1;
+
+import java.net.SocketOption;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        }
+    }
